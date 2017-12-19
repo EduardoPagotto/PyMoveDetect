02e23642b263607977eca1b632d3989a4aea0167
Eduardo Pagotto <edupagotto@gmail.com>
2017-12-19 19:33:05 -0200

    teste z1

--- move_detect.py
+++ move_detect.py
@@ -43,11 +43,10 @@ def get_image_name(path, prefix):
 def track2(vs, mv):
 
     grayimage1 = vs.get_gray_image()
-    if vs.canvas.windows_on:
-        logging.info('pressione q para sair')
-    else:
-        logging.info('precione crt-c para sair')
-
+    # if vs.canvas.windows_on:
+    #     logging.info('pressione q para sair')
+    # else:
+    #     logging.info('precione crt-c para sair')
 
     logging.info('A iniciar tracking...')
 
@@ -63,7 +62,8 @@ def track2(vs, mv):
         #FIXME mudei a ordem da aquisicao do get com o gray
         grayimage2 = vs.get_gray_image()#cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
 
-        mv.detect_area(grayimage1, grayimage2, move_time)
+        #mv.detect_area(grayimage1, grayimage2, move_time)
+        cv2.imshow('TESTE',grayimage2)
 
         grayimage1 = grayimage2
 
@@ -88,7 +88,7 @@ if __name__ == '__main__':
             #vs = VideoStreamDev(0, canvas_img.CAMERA_WIDTH, canvas_img.CAMERA_HEIGHT)
 
             vs.start()
-            time.sleep(4)
+            time.sleep(14)
 
             track2(vs, mv)
 
